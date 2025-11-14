2e05c7a9dfc0a9feccd39851b9136f59a5e6e455
vasavifullstack <vasavi.fullstack@gmail.com>
2025-11-14 16:57:41 +0530

    demo

---
+++ ConsoleApp1/8.operator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+ class Operator
+    {
+    static void Main()
+    {
+        //Arthamatic opertos   + , - , * , / 9:03
+        int result = 1003 + 2650;  // 3653 numeric
+        Console.WriteLine(result); // 30
+
+        int result1 = 20 - 100;  // -80 numeric
+        Console.WriteLine(result1); // 10
+
+        // == , != , < , > , <= , >=  // true or false 
+        bool isEqual = 10 == 10;
+        Console.WriteLine(isEqual); // true 
+
+        bool isEqual1 = 10 == 20;
+        Console.WriteLine(isEqual1); // false 
+
+        bool isEqual2 = 10 < 20;
+        Console.WriteLine(isEqual2); // false 
+        bool isEqual3 = 10 > 20;
+        Console.WriteLine(isEqual3); // false.
+        bool isEqual4 = (10 >= 20);
+        Console.WriteLine(isEqual4); // false 
+
+        bool isEqual5 = (10 <= 20);
+        Console.WriteLine(isEqual5); // false
+
+        // (3+4) < 6
+
+        //&& , || , !
+
+        //T t - t  , t f - f , f t - f , f  f - f
+
+
+        Console.WriteLine("-----------------AND--------------");
+
+        bool isResult = (10 == 10) && (20 == 20);
+        Console.WriteLine(isResult);
+
+        bool isResult1 = (10 == 10) && (30 == 20);
+        Console.WriteLine(isResult1);
+
+        bool isResult2 = (20 == 10) && (20 == 20);
+        Console.WriteLine(isResult2);
+
+        bool isResult3 = (20 == 10) && (10 == 20);
+        Console.WriteLine(isResult3);
+        //T t - t  , t f - t , f t - t , f  f - f
+
+
+        Console.WriteLine("----------------OR----------------");
+
+        bool isResult4 = (10 == 10) || (20 == 20);
+        Console.WriteLine(isResult4);
+
+        bool isResult5 = (10 == 10) || (30 == 20);
+        Console.WriteLine(isResult5);
+
+        bool isResult6 = (20 == 10) || (20 == 20);
+        Console.WriteLine(isResult6);
+
+        bool isResult7 = (20 == 10) || (10 == 20);
+        Console.WriteLine(isResult7);
+
+        Console.WriteLine("----------------NOT  !----------------");
+
+        bool isResult8 = !(10 == 10);  //false
+        Console.WriteLine(isResult8);
+
+        bool isResult9 = !(20 == 10);  //true
+        Console.WriteLine(isResult9);
+
+
+        // Ex : compare and Logical we will use in if else blocks
+
+
+
+
+
+    }
+}
