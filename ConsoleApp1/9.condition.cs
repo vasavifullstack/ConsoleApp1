5cc111a98fd9deba49d8a6cdb27fcb9f011b25d2
vasavifullstack <vasavi.fullstack@gmail.com>
2025-11-14 17:31:20 +0530

    condition

---
+++ ConsoleApp1/9.condition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+ class Condition
+    {
+    static void Main()
+    {
+        // operatos
+        // if statemnt else if statement  else statement 
+
+        bool result = (10 < 20);
+
+        string bankName = "SBI";
+
+        if (bankName == "HDFC")
+        {
+            Console.WriteLine("MY Password is MadanHDFC!1234");
+        }
+        else if (bankName == "Axis")
+        {
+            Console.WriteLine("MY Password is MadanAxi!1234");
+        }
+        else if (bankName == "SBI")
+        {
+            Console.WriteLine("MY Password is MadanSBI!1234");
+        }
+        else
+        {
+            Console.WriteLine("You do't have permission to see the password");
+        }
+
+
+    }
+}
